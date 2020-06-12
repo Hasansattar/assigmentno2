378313c13c806af96ad636ec5de6043acea3778e
hasansattar <hasansattar650@gmail.com>
2020-06-13 02:19:59 +0500

    updated app.js fils

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-       <h1>hello world no 1 hahahaha sahi hi gaya</h1>
+       <h1>hello world</h1>
+       <h2>MY NAME IS HASAN FROM FAISLABAD</h2>
+       <P>this is my second assigment</P>
     </div>
   );
 }
